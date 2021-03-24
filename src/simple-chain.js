c62acd6d1e8a2f970434c08828bdfaa2bf05ab38
ValeryiaMIRON <lapenko69valeria@mail.ru>
2021-03-24 22:40:09 +0300

    feat:add solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  arr : [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.arr.length;
+    return this
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (value === undefined) {
+        this.arr.push('( )')
+        }
+        this.arr.push(`( ${value} )`);
+        return this
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (typeof position !== "number" || position < 1) {
+      this.arr = []
+      throw new Error();
+      } 
+        this.arr.splice(position - 1,1)
+        return this
+        
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.arr.reverse()
+    return this
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+     let res = this.arr.join('~~')
+     this.arr =[]
+     return res
   }
 };
 
