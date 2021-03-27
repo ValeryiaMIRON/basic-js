e6058cd9756a07052e03183875082b30d28ebeec
ValeryiaMIRON <lapenko69valeria@mail.ru>
2021-03-27 18:56:43 +0300

    feat: add solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  if (typeof str != 'string') {
+    str = str + ''
+  }
+
+  if (!options.hasOwnProperty('addition')) {
+    options.addition = ''
+  }
+
+  if (typeof options.addition !== 'string') {
+    options.addition = options.addition + ''
+  }
+
+if (options.separator == undefined){
+  options.separator = '+'
+}
+if ( options.additionSeparator == undefined) {
+    options.additionSeparator = '|'
+  }
+
+  let first = str + options.addition
+
+  let second = (options.additionSeparator + options.addition).repeat(options.additionRepeatTimes - 1)
+  let three = (first + second + options.separator).repeat(options.repeatTimes - 1) 
+  let result = three + first + second
+  return result
+  
 };
   
